781676f6b66f3d3f7f0f226069b3b1bdba9974d0
Rafael Steffens <rafaelfsteffens@gmail.com>
2025-06-30 21:19:52 -0300

    Criei a corrida

--- Uber/src/main/java/com/Uber/Uber/Controllers/CreateChamados.java
+++ Uber/src/main/java/com/Uber/Uber/Controllers/CreateChamados.java
@@ -1,8 +1,8 @@
 package com.Uber.Uber.Controllers;
 
+import java.util.List;
 import java.util.Map;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,16 +12,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.Uber.Uber.Entitys.Chamados;
 import com.Uber.Uber.Entitys.Clientes;
-import com.Uber.Uber.Entitys.Endereco;
-import com.Uber.Uber.Entitys.Motoristas;
-import com.Uber.Uber.Entitys.Veiculos;
+import com.Uber.Uber.Entitys.Corrida;
 import com.Uber.Uber.Repositorys.ChamadosRepository;
 import com.Uber.Uber.Repositorys.ClientesRepository;
+import com.Uber.Uber.Repositorys.CorridaRepository;
 import com.Uber.Uber.Repositorys.EnderecoRepository;
 import com.Uber.Uber.Repositorys.MotoristasRepository;
 import com.Uber.Uber.Repositorys.VeiculosRepository;
+
 
 
 
@@ -46,67 +45,105 @@ public class CreateChamados {
     @Autowired
     private EnderecoRepository enderecoRepository;
 
+    @Autowired
+    private CorridaRepository corridaRepository;
+
     @GetMapping("/index")
     public String Index() {
         return "index";
     }
-    
-    @PostMapping("/createChamado")
-    public ResponseEntity<String> createNewChamado(@RequestBody String requestBody) {
-        JSONObject json = new JSONObject(requestBody);
+    @PostMapping("/createUser")
+    public ResponseEntity<String> formRegisterUser (@RequestBody Clientes request) {
 
-        createCorrida();
-            JSONObject cliente = json.getJSONObject("cliente");
-            JSONObject endereco = cliente.getJSONObject("endereco");
+        Clientes newCliente = new Clientes(request.getNome(), request.getTelefone(), request.getEndereco(), request.getCpfRg());
+
+        clientesRepository.save(newCliente);
+
         
-            String logradouro = endereco.getString("logradouro");
-            int numero = endereco.getInt("numero");
-            String cep = endereco.getString("cep");
-            String complemento = endereco.getString("complemento");
-            String estado = endereco.getString("estado");
-            String cidade = endereco.getString("cidade");
-        
-            Endereco enderecoCliente = new Endereco(logradouro, numero, cep, complemento, estado, cidade);
-            enderecoRepository.save(enderecoCliente);
-        
-            // Criar cliente com os dados também do JSON, por exemplo:
-            String nome = cliente.getString("nome");
-            String telefone = cliente.getString("telefone");
-            String cpfRg = cliente.getString("cpfRg");
-        
-            Clientes clienteObj = new Clientes(nome, telefone, enderecoCliente, cpfRg);
-            clientesRepository.save(clienteObj);
-
-
-
-        buscaMotorista();
-            // Motorista
-            Endereco enderecoMotorista = new Endereco("Rua A", 123, "88000000", "Casa", "SC", "Florianópolis");
-            enderecoRepository.save(enderecoMotorista);
-            Motoristas motorista = new Motoristas("Rafael", "489917717665", enderecoMotorista, "788995562");
-            motoristasRepository.save(motorista);
-            // Veículo
-            Veiculos veiculo = new Veiculos("OZW-6777", "F-80", "2025", "Vermelha", "Ferrari");
-            veiculosRepository.save(veiculo);
-
-        // Chamado
-        Chamados chamado = new Chamados(
-            "Rio Grande do Sul",
-            "Santa Catarina",
-            "2025-06-23",
-            "Uber X",
-            1020.0,
-            1028.3,
-            "21:00",
-            "21:20",
-            27.0,
-            veiculo, motorista, clienteObj
-        );
-
-        chamadosRepository.save(chamado);
-
-        return ResponseEntity.ok("Chamado criado com sucesso!");
+        return ResponseEntity.ok("Cliente Cadastrado com sucesso");
     }
+
+    @PostMapping("/createCorrida")
+    public ResponseEntity<String> createCorrida(@RequestBody Corrida request) {
+
+        Corrida newCorrida = new Corrida(request.getOrigem(), request.getDestino(), request.getData(), request.getTipoServico(), request.getIdCliente());
+
+        corridaRepository.save(newCorrida);
+
+
+        //TODO: process POST request
+        
+        return ResponseEntity.ok("Corrida Cadastradaa com sucesso");
+    }
+
+    
+    @GetMapping("/consultaCorrida")
+    public List<Corrida> consultaCorrida() {
+        return corridaRepository.findAll();
+    }
+    
+
+
+    
+
+    
+    
+    // @PostMapping("/createChamado")
+    // public ResponseEntity<String> createNewChamado(@RequestBody String requestBody) {
+    //     // JSONObject json = new JSONObject(requestBody);
+
+    //     // createCorrida();
+    //     //     JSONObject cliente = json.getJSONObject("cliente");
+    //     //     JSONObject endereco = cliente.getJSONObject("endereco");
+        
+    //     //     String logradouro = endereco.getString("logradouro");
+    //     //     int numero = endereco.getInt("numero");
+    //     //     String cep = endereco.getString("cep");
+    //     //     String complemento = endereco.getString("complemento");
+    //     //     String estado = endereco.getString("estado");
+    //     //     String cidade = endereco.getString("cidade");
+        
+    //     //     Endereco enderecoCliente = new Endereco(logradouro, numero, cep, complemento, estado, cidade);
+    //     //     enderecoRepository.save(enderecoCliente);
+        
+    //     //     // Criar cliente com os dados também do JSON, por exemplo:
+    //     //     String nome = cliente.getString("nome");
+    //     //     String telefone = cliente.getString("telefone");
+    //     //     String cpfRg = cliente.getString("cpfRg");
+        
+    //     //     Clientes clienteObj = new Clientes(nome, telefone, enderecoCliente, cpfRg);
+    //     //     clientesRepository.save(clienteObj);
+
+
+
+    //     // buscaMotorista();
+    //     //     // Motorista
+    //     //     Endereco enderecoMotorista = new Endereco("Rua A", 123, "88000000", "Casa", "SC", "Florianópolis");
+    //     //     enderecoRepository.save(enderecoMotorista);
+    //     //     Motoristas motorista = new Motoristas("Rafael", "489917717665", enderecoMotorista, "788995562");
+    //     //     motoristasRepository.save(motorista);
+    //     //     // Veículo
+    //     //     Veiculos veiculo = new Veiculos("OZW-6777", "F-80", "2025", "Vermelha", "Ferrari");
+    //     //     veiculosRepository.save(veiculo);
+
+    //     // // Chamado
+    //     // Chamados chamado = new Chamados(
+    //     //     "Rio Grande do Sul",
+    //     //     "Santa Catarina",
+    //     //     "2025-06-23",
+    //     //     "Uber X",
+    //     //     1020.0,
+    //     //     1028.3,
+    //     //     "21:00",
+    //     //     "21:20",
+    //     //     27.0,
+    //     //     veiculo, motorista, clienteObj
+    //     // );
+
+    //     // chamadosRepository.save(chamado);
+
+    //     // return ResponseEntity.ok("Chamado criado com sucesso!");
+    // }
 
     public  void createCorrida(){
 
